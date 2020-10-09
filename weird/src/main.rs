1954d7a4bb74fab730f935e2c982c2a5ad324a54
darkgallium <darkgallium@deuxfleurs.fr>
2020-10-09 15:25:11 +0200

    chall 1

--- weird/src/main.rs
+++ weird/src/main.rs
@@ -1,3 +1,18 @@
+use std::io::stdin;
+
 fn main() {
-    
+    let mut line = String::new();
+    stdin().read_line(&mut line).unwrap();
+    let mut i: u64 = line.trim().parse().unwrap();
+    print!("{}", i);
+
+    while i != 1 {
+        if i % 2 == 0 {
+            i = i/2;
+        } else {
+            i = i*3 + 1;
+        }
+
+        print!(" {}", i);
+    }
 }
